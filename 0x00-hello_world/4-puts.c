081fa017035c705908aab794f01424933226261c
Ian Culp <icculp@gmail.com>
2020-02-05 19:25:31 +0000

    Readding 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main(){
-        puts("\"Programming is like building a multilingual puzzle");
-        return 0;
+/**
+* main - Just a project
+* Description: Just a project
+* Return: All is working
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
