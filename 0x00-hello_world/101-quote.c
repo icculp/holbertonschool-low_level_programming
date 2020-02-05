ab946298cc3717e894d629a0027ef66b346f4236
Ian Culp <icculp@gmail.com>
2020-02-05 21:12:45 +0000

    Readding 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 /**
 * main - Entry
 * Return: Failure
 */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 	return (1);
 }
