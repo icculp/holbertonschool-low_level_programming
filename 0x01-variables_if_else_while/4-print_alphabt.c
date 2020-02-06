bcf567cc7688eea1b6073c492927ec689737e198
Ian Culp <icculp@gmail.com>
2020-02-06 21:25:01 +0000

    Adding 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - Entry
+* Return: Success
+*/
+
+int main(void)
+{
+	int i;
+	char c, n;
+
+	c = 'a';
+	n = '\n';
+
+	for (i = 0; i < 26; i++)
+	{
+		if (i == 4 || i == 16)
+		{
+			c++;
+		}
+		else
+		{
+			putchar(c);
+			c++;
+		}
+	}
+
+	putchar(n);
+
+	return (0);
+}
