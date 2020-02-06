6f61572ed0d3855280baf70bf5e6af9e80d65bba
Ian Culp <icculp@gmail.com>
2020-02-06 21:37:31 +0000

    Adding 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - Entry
+* Return: Success
+*/
+
+int main(void)
+{
+	int i;
+	char c, n;
+
+	c = '0';
+	n = '\n';
+
+	for (i = 0; i < 10; i++)
+	{
+
+		putchar(c);
+		c++;
+
+	}
+
+	putchar(n);
+
+	return (0);
+}
