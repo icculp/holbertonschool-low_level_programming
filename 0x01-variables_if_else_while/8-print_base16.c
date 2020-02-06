5e297941583dcefe5340b69e18f4a4e874afaffd
Ian Culp <icculp@gmail.com>
2020-02-06 22:16:48 +0000

    Adding 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - Entry
+* Return: Success
+*/
+
+int main(void)
+{
+	int i, c, n;
+
+	n = 48;
+	c = 97;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(n);
+		n++;
+	}
+	for (i = 0; i < 6; i++)
+	{
+
+		putchar(c);
+		c++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
