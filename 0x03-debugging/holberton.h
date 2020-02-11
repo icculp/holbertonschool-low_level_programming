78b9af16b700377779b86d9bf0dbe3de02654357
Ian Culp <icculp@gmail.com>
2020-02-11 20:37:23 +0000

    Adding 2

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -5,11 +5,6 @@
 
 void positive_or_negative(int i);
 
-#endif /* HOLBERTON_H */#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-#include <stdio.h>
-
-void positive_or_negative(int i);
-
 #endif /* HOLBERTON_H */
+int largest_number(int a, int b, int c);
+
