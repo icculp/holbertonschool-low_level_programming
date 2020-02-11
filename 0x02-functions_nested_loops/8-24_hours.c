aff8ec9361ac9fe55fc38e32dc16cbf3c24a7654
Ian Culp <icculp@gmail.com>
2020-02-11 16:57:20 +0000

    Readding 8, fixed seconds

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,24 +9,18 @@
 
 void jack_bauer(void)
 {
-	int hours = 0, minutes = 0, seconds = 0;
+	int hours = 0, minutes = 0;
 
 	for (hours = 0; hours < 24; hours++)
 	{
 		for (minutes = 0; minutes < 60; minutes++)
 		{
-			for (seconds = 0; seconds < 60; seconds++)
-			{
 				_putchar('0' + (hours / 10));
 				_putchar('0' + (hours % 10));
 				_putchar(':');
 				_putchar('0' + (minutes / 10));
 				_putchar('0' + (minutes % 10));
-				_putchar(':');
-				_putchar('0' + (seconds / 10));
-				_putchar('0' + (seconds % 10));
 				_putchar('\n');
-			}
 		}
 	}
 }
