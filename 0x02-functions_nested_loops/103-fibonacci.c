153e20dfd81de67cc52a04ba0f2b60e05d87c248
Ian Culp <icculp@gmail.com>
2020-02-12 02:55:23 +0000

    Adding 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+* main - Prints the first 50 numbers of fibonacci series
+*
+* Return: Void
+*/
+
+int main(void)
+{
+	long int i, j, k, l, evensum = 0;
+
+	j = 1;
+	k = 2;
+
+	for (i = 0; i < 32; i++)
+	{
+		if (i < 31)
+		{
+			if (j % 2 == 0)
+			{
+				evensum += j;
+			}
+		l = j + k;
+		j = k;
+		k = l;
+		}
+	}
+	printf("%ld\n", evensum);
+	return (0);
+}
