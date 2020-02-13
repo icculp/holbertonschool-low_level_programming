c0d49727bd63314d69054877f85ba5f782f580c9
Ian Culp <icculp@gmail.com>
2020-02-13 19:58:37 +0000

    Only printing largest prime for 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -27,14 +27,14 @@ void primeFactors(long int n)
 
 	while (n % 2 == 0)
 	{
-	printf("%d, ", 2);
+	/** printf("%d, ", 2);*/
 	n = n / 2;
 	}
 	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
 		{
-		printf("%d, ", i);
+/**		printf("%d, ", i);*/
 		n = n / i;
 		}
 	}
