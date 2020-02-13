c4c9d5922721be7a1f3121457e46a67069df4566
Ian Culp <icculp@gmail.com>
2020-02-13 18:44:56 +0000

    Fixed space after Buzz in 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,6 +12,8 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
+		if (i < 100)
+		{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
@@ -20,7 +22,9 @@ int main(void)
 			printf("Buzz ");
 		else
 			printf("%d ", i);
+		}
+		else
+			printf("Buzz\n");
 	}
-	printf("\n");
 	return (0);
 }
