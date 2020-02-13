c8c794468600a1a03b379fe3e536006ee92d0d0d
Ian Culp <icculp@gmail.com>
2020-02-13 19:51:57 +0000

    Adding 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+* main - Pulls and prints primes for given number
+* Return: Success
+* primeFactors - Stuff
+* @n: Stuff
+*/
+
+void primeFactors(long int n);
+
+int main(void)
+{
+	primeFactors(612852475143);
+	return (0);
+}
+
+/**
+* primeFactors - Prints all primes of a given number
+* @n: Number to pull prime factors out of
+*/
+
+void primeFactors(long int n)
+{
+	int i;
+
+	while (n % 2 == 0)
+	{
+	printf("%d ", 2);
+	n = n / 2;
+	}
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+		printf("%d ", i);
+		n = n / i;
+		}
+	}
+	if (n > 2)
+		printf("%li ", n);
+
+	printf("\n");
+}
