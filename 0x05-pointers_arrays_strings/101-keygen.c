7853ce4d514ad6efc178934712a8270c3db97c3a
Ian Culp <icculp@gmail.com>
2020-02-19 02:37:31 +0000

    Adding last project

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,18 +4,19 @@
 
 /**
 * main - Creates a random password with asci value sum of 2772
+* Return: Success
 */
 
 
 int main(void)
 {
 	int i, lastc = 0;
-	char asci[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	char asci[] = "abcdefghijklmnopqrstuvwxyzBCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 	srand(time(0));
-	while (lastc < 2772 -122)
+	while (lastc < 2772 - 122)
 	{
-		i = rand() % 86;
+		i = rand() % 61;
 		lastc += asci[i];
 		printf("%c", asci[i]);
 	}
