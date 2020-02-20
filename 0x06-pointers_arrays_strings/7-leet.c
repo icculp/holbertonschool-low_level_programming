fee846818d62a117f150b0d1439d60c59763533b
Ian Culp <icculp@gmail.com>
2020-02-20 22:02:03 +0000

    Adding 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+* Leet - Converts string into leetspeak
+* @ch: String to convert
+* Return: Converted string
+*/
+
+char *leet(char *ch)
+{
+	int i = 0, j;
+	int r[] = {'a','A','e','E','o','O','t','T','l','L'};
+	int l[] = {'4','4','3','3','0','0','7','7','1','1'};
+
+	while (ch[i])
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (ch[i] == r[j])
+				ch[i] = l[j];
+		}
+		i++;
+	}
+	return (ch);
+}
