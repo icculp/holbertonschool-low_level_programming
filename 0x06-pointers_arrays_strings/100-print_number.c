4913a8bfef4dc7df7f92200d5542abb6caaa675f
Ian Culp <icculp@gmail.com>
2020-02-20 22:37:02 +0000

    Adding 100

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+* print_number - Prints an integer
+* @n: Integer to print
+*/
+
+void print_number(int n)
+{
+	unsigned int temp, factor = 1;
+
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+	temp = n;
+	while (temp > 9)
+	{
+		temp = temp / 10;
+		factor = factor * 10;
+	}
+	while (factor >= 1)
+	{
+		_putchar((n / factor) + '0');
+		n = n % factor;
+		factor = factor / 10;
+	}
+	}
+}
