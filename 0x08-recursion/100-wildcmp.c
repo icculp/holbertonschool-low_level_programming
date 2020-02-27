42a8ddb08ee2a73855713eaf90349e796309c753
Ian Culp <icculp@gmail.com>
2020-02-27 04:48:10 +0000

    Adding empty 100

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+
+/**
+* wildcmp - Compares two strings
+* @s1: First string to compare
+* @s2: Second string to compare
+* Return: Zero or One
+*/
+int wildcmp(char *s1, char *s2)
+{
+	return (0);
+}
