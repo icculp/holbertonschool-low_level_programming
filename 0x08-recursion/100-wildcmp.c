952066eaead3c0d3c0925b77636bc1d6765b8ffe
Ian Culp <icculp@gmail.com>
2020-02-28 00:30:09 +0000

    Adding 100 skeleton

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,5 +1,27 @@
 #include "holberton.h"
 
+/**
+* compare - Compares the original string with the reversed string
+* @s: Source string
+* @rev: Reversed string
+* @i: Iterator
+* Return: 1 if equal 0 if not
+*/
+
+int compare(char *s, char *rev, int i)
+{
+	if (s[i] != '\0')
+	{
+	if (*(s + i) == *(rev + i))
+		return (compare(s, rev, i + 1));
+	else if (*(s + i) != *(rev + i))
+		return (0);
+	else
+		return (1);
+	}
+	else
+		return (1);
+}
 
 /**
 * wildcmp - Compares two strings
@@ -7,7 +29,8 @@
 * @s2: Second string to compare
 * Return: Zero or One
 */
+
 int wildcmp(char *s1, char *s2)
 {
-	return (0);
+	return (compare(s1, s2, 0));
 }
