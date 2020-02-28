734701b333393caa14001f5aeee38721c1f4795b
Ian Culp <icculp@gmail.com>
2020-02-28 21:18:24 +0000

    Fixing 100 for 2-cent piece

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	int c;
 	int i = 0, change = 0, remainder, count = 0;
-	int cents[] = {25, 10, 5, 1};
+	int cents[] = {25, 10, 5, 2, 1};
 
 
 	if (argc != 2)
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 			printf("0\n");
 			return (0);
 		}
-		for (i = 0; i < 4; i++)
+		for (i = 0; i < 5; i++)
 		{
 			if (remainder >= cents[i])
 			{
