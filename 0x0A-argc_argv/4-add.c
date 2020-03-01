638b6d5b9d8a0763563e820153a7f3b5b88bb06f
Ian Culp <icculp@gmail.com>
2020-03-01 03:41:32 +0000

    Fixing 3 for (12e) num with character problem with atoi

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+* verify - Checks if the entire string is an int or has characters
+* @string: String to check
+* Return: 0 on success, 1 on failure.
+*/
+
+int verify(char *string)
+{
+	int x = 0;
+	int len = strlen(string);
+
+	while (x < len)
+	{
+		if (!isdigit(*(string + x)))
+			return (1);
+		++x;
+	}
+	return (0);
+}
 
 /**
 * main - Prints number of arguments passed to function
@@ -14,11 +36,11 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) > 0)
+		if (verify(argv[i]) == 0)
 		{
 			sum += atoi(argv[i]);
 		}
-		else if (atoi(argv[i]) < 1)
+		else if (verify(argv[i]) == 1)
 		{
 			printf("Error\n");
 			return (1);
