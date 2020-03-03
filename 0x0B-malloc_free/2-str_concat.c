92871999d0fddd305a6ee8bddd537ef854bcb7ec
Ian Culp <icculp@gmail.com>
2020-03-03 19:22:10 +0000

    Adding code into 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,6 +3,76 @@
 #include <stdlib.h>
 
 /**
+* *_strcpy - Copies a string pointed to by src including null char to buffer
+* @dest: A buffer to receive the copied string
+* @src: The pointer of the string to the copied
+* Return: Value of buffer dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0, j;
+
+	while (src[i] != '\0')
+		i++;
+	i++;
+	for (j = 0; j < i; j++)
+		dest[j] = src[j];
+
+	return (dest);
+}
+
+/**
+* _strcat - Concatenates two strings
+* @dest: Character buffer
+* @src: Character source
+* Return: On success 1.
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0, k = 0;
+
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	i++;
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+	for (k = 0; k < (i); j++, k++)
+	{
+		dest[j] = src[k];
+	}
+	return (dest);
+}
+
+/**
+* _strlen - Determines the length of the string manually
+* @s: Pointer to array of characters
+* Return: Value of length
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+/**
+* _strcat - Concatenates two strings
+* @dest: Character buffer
+* @src: Character source
+* Return: On success 1.
+*/
+
+
+/**
 * *str_concat - Concatenates two strings with memeory in mind
 * @s1: String one to concat
 * @s2: String two to contat to s1
