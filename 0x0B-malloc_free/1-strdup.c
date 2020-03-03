655eae0063e16df852839869a49467b559cd0b4b
Ian Culp <icculp@gmail.com>
2020-03-03 18:52:09 +0000

    Added code into file for strlen for checker

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,6 +3,22 @@
 #include <stdlib.h>
 
 /**
+* _strlen - Determines the length of the string manually
+* @s: Pointer to array of characters
+* Return: Value of length
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
 * *_strdup - Returns a pointer to new memory space containing copy of string
 * @str: String to copy into new memory spot
 * Return: Pointer to duplicate string, or NULL if not enough memory
