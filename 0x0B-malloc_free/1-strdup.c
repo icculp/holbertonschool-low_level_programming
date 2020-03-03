daf9bde6c57740dae0714bb7c532ecfacba3cf2a
Ian Culp <icculp@gmail.com>
2020-03-03 19:10:07 +0000

    I don't know

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -29,10 +29,10 @@ char *_strdup(char *str)
 	char *ray;
 	int i = 0;
 
-	ray = (char*)malloc(_strlen(str) + 1);
+	ray = (char *) malloc(_strlen(str) + 1);
 	if (ray == NULL || str == NULL)
 		return (NULL);
-	while (i < _strlen(str))
+	while (i <= _strlen(str))
 	{
 		ray[i] = str[i];
 		i++;
