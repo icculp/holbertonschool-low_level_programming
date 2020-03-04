3bdf0ccb92b1a7255d93f0320f1b3fcc252aeb7b
Ian Culp <icculp@gmail.com>
2020-03-04 20:40:16 +0000

    Adding 100 with working code but one extra line

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -3,6 +3,35 @@
 #include <stdlib.h>
 
 /**
+* wordcount - Counts number of words
+* @str: String to count words
+* Return: Count of words
+*/
+int wordcount(char *str)
+{
+	int count = 0;
+
+	if (*str == '\0')
+		return (0);
+	while (*str != '\0')
+	{
+		while (*str == ' ')
+		{
+		str++;
+		}
+		count++;
+		while (*str != ' ')
+		{
+			if (*str != '\0')
+				str++;
+			else
+				break;
+		}
+	}
+	return (count);
+}
+
+/**
 * _strlen - Determines the length of the string manually
 * @s: Pointer to array of characters
 * Return: Value of length
@@ -27,28 +56,49 @@ int _strlen(char *s)
 char **strtow(char *str)
 {
 	char **ray;
-	int i = 0, j = 0, k = 0;
+	int i = 0, j = 0, wc = 0, inlen = 0, totlen = 0;
 
 	if (str == NULL || str[0] == '\0')
 		return (NULL);
-	ray = (char **) malloc(100);
+	wc = wordcount(str);
+	ray = (char **) malloc(wc * sizeof(char *)); 
 	if (ray == NULL)
 	{
 		free(ray);
 		return (NULL);
 	}
-	while (1)
+	for (i = 0; i < wc; i++)
 	{
-		if (str[i] != ' ')
+		for (inlen = 0; str[totlen + inlen] != ' '; inlen++)
+			;
+		ray[i] = (char *) malloc(sizeof(char) * inlen);
+		if (ray[i] == NULL)
 		{
-			ray[i] = (char *) malloc(200);
-			if (ray[i] == NULL)
-			{
-				for (; i >= 0; i--)
-					free(ray[i]);
-				free(ray);
-				return (NULL);
-			}
+			for (; i >= 0; i--)
+				free(ray[i]);
+			free(ray);
+			return (NULL);
+		}
+		for (j = 0; j < inlen; j++, totlen++)
+		{
+			ray[i][j] = str[totlen];
+		}
+		if (str[totlen] != ' ')
+		{
+			if (i < wc)
+				ray[i][j] = '\n';
+			else
+				ray[i][j] = '\0';
+		}
+		while (str[totlen] == ' ')
+			totlen++;
+		if (str[totlen] == '\0')
+			break;
+	}
+	return (ray);
+/**
+		if (str[k] != ' ')
+		{
 			ray[i][j++] = str[k++];
 		}
 		else
@@ -60,9 +110,9 @@ char **strtow(char *str)
 			}
 			else
 			{
-				i++, k++, j = 0;
+				i++, j = 0;
 			}}
-		if (str[i] == '\0')
-			break; }
-	return (ray);
+		if (str[k] == '\0')
+			break; } k++;
+	return (ray);*/
 }
