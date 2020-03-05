0ba4253434faa61cf1cbc281a64f4ce7efd806b1
Ian Culp <icculp@gmail.com>
2020-03-05 19:44:31 +0000

    Closing )

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -109,7 +109,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n < (unsigned int)_strlen(s2))
 		ray = _strncat(ray, s2, n);
 	else
-		ray = _strncat(ray, s2, _strlen(s2);
+		ray = _strncat(ray, s2, _strlen(s2));
 
 	return (ray);
 }
