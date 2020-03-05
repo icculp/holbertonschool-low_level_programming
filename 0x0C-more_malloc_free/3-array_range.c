a9b1efe0176c249966747c715c392cb1bae0b63a
Ian Culp <icculp@gmail.com>
2020-03-05 21:24:23 +0000

    Iterating to numvals(max-min) when should've iterated to max

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 	if (ray == NULL)
 		return (NULL);
 
-	for (; mincpy <= numvals; mincpy++, index++)
+	for (; mincpy <= max; mincpy++, index++)
 	{
 		ray[index] = mincpy;
 	}
