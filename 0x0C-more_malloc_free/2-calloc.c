039b657c93745dabc92ce042683329f4c1807e5b
Ian Culp <icculp@gmail.com>
2020-03-05 21:02:29 +0000

    If nmemb or size equals 0, return 0 before malloc call

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,9 +14,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ray;
 	unsigned int i, j = 0;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 	j = (size * nmemb);
 	ray = malloc(j);
-	if (ray == NULL || nmemb == 0 || size == 0)
+	if (ray == NULL)
 		return (NULL);
 
 	for (i = 0; i < j; i++)
