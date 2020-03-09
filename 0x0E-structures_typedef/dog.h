015c74ee97479452dda5f013d6ad433c39b86840
Ian Culp <icculp@gmail.com>
2020-03-09 20:07:08 +0000

    Adding 0x0E

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - Struct for a dog
+* @name: Doggo's name
+* @age: Doggo's age
+* @owner: Doggo's owner
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
