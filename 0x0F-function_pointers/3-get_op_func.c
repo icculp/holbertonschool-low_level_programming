40848c92f22b23de51812553fe6acb7295532a98
Ian Culp <icculp@gmail.com>
2020-03-11 22:08:27 +0000

    Changing if NULL function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,7 +18,7 @@ int (*get_op_func(char *s))(int, int)
 			};
 	int i;
 
-	for (i = 0; i < 6; i++)
+	for (i = 0; i < 5; i++)
 	{
 		if ((strcmp(s, ops[i].op)) == 0)
 			return (ops[i].f);
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -29,6 +29,6 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", (*operator)(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", operator(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
