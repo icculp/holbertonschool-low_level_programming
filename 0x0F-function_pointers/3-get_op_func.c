cd3440b522c6d69da143827d6851e6fb90a37fa6
Ian Culp <icculp@gmail.com>
2020-03-11 22:16:40 +0000

    Changed for loop to while loop

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,10 +18,11 @@ int (*get_op_func(char *s))(int, int)
 			};
 	int i;
 
-	for (i = 0; i < 5; i++)
+	while (ops[i].op != NULL)
 	{
 		if ((strcmp(s, ops[i].op)) == 0)
 			return (ops[i].f);
+		i++;
 	}
 	return (ops[i].f);
 }
