b87e4477c467f6ec706b6042f8330fa979b6eea7
Ian Culp <icculp@gmail.com>
2020-03-19 19:08:37 +0000

    Fixing 2 without strlen and error check after malloc

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,10 +10,15 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	int len = 0;
 
+	while (str[len])
+		len++;
 	new = (list_t *) malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
 	new->str = strdup(str);
-	new->len = strlen(str);
+	new->len = len;
 	new->next = *head;
 	*head = new;
 
