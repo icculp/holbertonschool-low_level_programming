a949d257cefc78ac5d2c129d2c896d0bfef78882
Ian Culp <icculp@gmail.com>
2020-03-19 21:41:33 +0000

    Adding 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,8 +4,10 @@
 * pb4main - Prints a sentence before main executes
 */
 
-void pb4main (void) __attribute__ ((constructor))
+void pb4main (void) __attribute__ ((constructor));
+
+void pb4main (void)
 {
-	printf("You're beat! and yet, you must allow,\n
-		I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
