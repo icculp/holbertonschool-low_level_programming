7887decabe1637779432c407b81c42be5fba19b3
Ian Culp <icculp@gmail.com>
2020-03-19 21:44:16 +0000

    Fixing betty on 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,9 +4,9 @@
 * pb4main - Prints a sentence before main executes
 */
 
-void pb4main (void) __attribute__ ((constructor));
+void pb4main(void) __attribute__((constructor));
 
-void pb4main (void)
+void pb4main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
