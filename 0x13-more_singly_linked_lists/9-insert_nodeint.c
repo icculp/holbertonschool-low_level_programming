5531720a50e279da97f4a1d66f1d6c18e31ef6ef
Ian Culp <icculp@gmail.com>
2020-03-24 17:49:40 +0000

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	if (head == NULL)
-		return (0);
+		return (NULL);
 
 	current = *head;
 	new = *head;
