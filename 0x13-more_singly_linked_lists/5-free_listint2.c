0856d21f6846fe9f0adbd6c63c7407be6339ea18
Ian Culp <icculp@gmail.com>
2020-03-24 17:05:50 +0000

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current, *next;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return;
 	current = *head;
 	while (current != NULL)
