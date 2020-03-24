7a2347b51b2ec614a4a28fa232ef5c863f77e814
Ian Culp <icculp@gmail.com>
2020-03-24 19:14:12 +0000

    Adding fix for index 0 for 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,8 +17,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	current = *head;
-	new = *head;
-
+	if (idx == 0)
+	{
+		new = malloc(sizeof(listint_t));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		new->next = current;
+		*head = new;
+		return (new);
+	}
 	for (i = 0; i < idx - 1; i++)
 	{
 		current = current->next;
