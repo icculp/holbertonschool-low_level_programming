4124f9bee1bad6203584679eedfa586efdfe12d6
Ian Culp <icculp@gmail.com>
2020-03-25 03:57:51 +0000

    Adding 102

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+* print_listint_safe - Prints a listint_t linked list
+* @head: Address of listint_t list
+* Return: Number of nodes in the list
+*/
+
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *temp = head;
+	size_t nodes;
+
+	if (head == NULL)
+		exit (98);
+	for (nodes = 0; temp != NULL; nodes++)
+	{
+		printf("[%p] %d\n", (void *)temp, temp->n);
+		if (temp->next >= temp)
+		{
+			printf("-> [%p] %d\n", (void *)temp->next, temp->next->n);
+			return (nodes);
+		}
+		temp = temp->next;
+	}
+
+	return (nodes);
+}
