72d5b0c88c302478d410ef050c438b9da7db11a4
Ian Culp <icculp@gmail.com>
2020-03-25 02:54:45 +0000

    Adding 100

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+* reverse_listint - Reverses a sinkly linked list
+* @head: Address of listint_t list
+* Return: Reversed list
+*/
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL, *next;
+
+
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (NULL);
+
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
+	return (prev);
+/***
+*	prev = reverse_listint(&(*head)->next);
+*	(*head)->next->next = *head;
+*	(*head)->next = NULL;
+*	return (prev);
+*/
+}
