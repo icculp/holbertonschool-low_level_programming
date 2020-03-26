3a146a9f8a96c12f0b75837f1fd08065fb392ada
Ian Culp <icculp@gmail.com>
2020-03-26 21:03:19 +0000

    Adding 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <math.h>
+
+/**
+* clear_but - Sets bit to 0 at given index
+* @n: Pointer to number to set bit of
+* @index: Index of bit to set to 1
+* Return: 1 on success, -1 if error
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 31)
+		return (-1);
+	*n &= ~(1UL << index);
+	return (1);
+}
