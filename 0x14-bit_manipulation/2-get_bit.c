cf9d996f0cf339ff9f9d7937f9dc02a305d9e94e
Ian Culp <icculp@gmail.com>
2020-03-26 20:13:57 +0000

    Adding 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <math.h>
+
+/**
+* get_bit - Returns value of a bit at given index
+* @n: Deciman number to find bit index of
+* @index: Index of bit to return
+* Return: Bit 1 or 0, or -1 if error
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int mask = 1, i = 0;
+
+	for (i = 0; i < index; i++)
+		mask = mask << 1;
+	if ((n & mask))
+		return (1);
+	else if (!(n & mask))
+		return (0);
+	else
+		return (-1);
+}
