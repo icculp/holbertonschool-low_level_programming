d68a68ef4ed11ebaefaabe9696024b45b0fc01c0
Ian Culp <icculp@gmail.com>
2020-03-26 20:48:36 +0000

    Adding 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <math.h>
+
+/**
+* set_bit - Sets bit to 1 at given index
+* @n: Pointer to number to set bit of
+* @index: Index of bit to set to 1
+* Return: 1 on success, -1 if error
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	*n |= 1UL << index;
+	return (*n);
+}
