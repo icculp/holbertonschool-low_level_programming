2cae807069c61310b23a11619fe64cf8e04e7a04
Ian Culp <icculp@gmail.com>
2020-03-31 23:46:27 +0000

    Typo in error message

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,7 +17,10 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	o_from = open(argv[1], O_RDONLY);
 	if (o_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	o_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (o_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
