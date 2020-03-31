05318792f8781e9b1da1127f4a25f59a0bd0385f
Ian Culp <icculp@gmail.com>
2020-03-31 18:53:26 +0000

    Freee's for all

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,16 +19,28 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	o = open(filename, O_RDONLY);
 	if (o == -1)
+	{
+		free(buf);
 		return (0);
+	}
 	r = read(o, buf, letters);
 	if (r == -1)
+	{
+		free(buf);
 		return (0);
+	}
 	buf[r] = '\0';
 	w = write(STDOUT_FILENO, buf, strlen(buf));
 	if ((w == -1) | (w == 0))
+	{
+		free(buf);
 		return (0);
+	}
 	if (r != w)
+	{
+		free(buf);
 		return (0);
+	}
 	free(buf);
 	close(o);
 	return (w);
