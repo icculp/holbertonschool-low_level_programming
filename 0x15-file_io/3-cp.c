eabcec3e8d9a8b9470ed3f875ef1abdac18849fc
Ian Culp <icculp@gmail.com>
2020-04-01 02:09:00 +0000

    Trying to help Kelsie

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	o_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	o_to = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
 	if (o_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	while ((len = read(o_from, buf, 1024)))
