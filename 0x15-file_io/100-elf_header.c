41d0a672abfdac252b4320fb325c256b07e171cb
Ian Culp <icculp@gmail.com>
2020-04-01 04:00:34 +0000

    Better 100

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,39 +1,34 @@
 #include <elf.h>
 #include "holberton.h"
 
-#include <elf.h>
-#include <stdio.h>
-
-void read_elf_header(const char* elfFile) {
-  // switch to Elf32_Ehdr for x86 architecture.
-  Elf64_Ehdr header;
-
-  FILE* file = fopen(elfFile, "rb");
-  if(file) {
-    // read the header
-    fread(&header, 1, sizeof(header), file);
-
-    // check so its really an elf file
-    if (memcmp(header.e_ident, ELFMAG, SELFMAG) == 0) {
-       // this is a valid elf file
-    }
-
-    // finally close the file
-    fclose(file);
-  }
-}
-
 /**
 * main - Displays info contained in ELF header
 */
 
 int main(int argc, char **argv)
 {
-	int o;
+	int o, r, c;
 	Elf64_Ehdr header;
 
 	if (argc != 2)
 		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n"), exit(98);
-	
+	o = open(argv[1], O_RDONLY);
+	if (o == -1)
+		dprintf(STDERR_FILENO, "Your file sucks\n"), exit(98);
+	r = read(o, &header, sizeof(header));
+	if (r == -1)
+		dprintf(STDERR_FILENO, "Your file sucks\n"), exit(98);
+	if (header.e_type == 0x7f &&
+		header.e_ident[1] == 'E' &&
+		header.e_ident[2] == 'L' &&
+		header.e_ident[3] == 'F')
+	{
+		printf("ELF Header:\n");
+	}
+	else
+		dprintf(STDERR_FILENO, "Your file sucks\n"), exit(98);
+	c = close(o);
+	if (c == -1)
+		dprintf(STDERR_FILENO, "Can't close this m'f, shit"), exit(98);
 	return (0);
 }
