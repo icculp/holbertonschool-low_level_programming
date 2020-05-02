a118f9c72cdccc786db5786f64a5cabc8f858bfa
Ian Culp <icculp@gmail.com>
2020-05-02 18:06:46 +0000

    Fixing memleak if idnex not found, 7

--- 0x17-doubly_linked_lists/7-main.c
+++ 0x17-doubly_linked_lists/7-main.c
@@ -11,9 +11,10 @@
 int main(void)
 {
     dlistint_t *head;
-    dlistint_t *node;
 
     head = NULL;
+    insert_dnodeint_at_index(&head, 0, 4096);
+    insert_dnodeint_at_index(&head, 2, 4091);
     add_dnodeint_end(&head, 0);
     add_dnodeint_end(&head, 1);
     add_dnodeint_end(&head, 2);
@@ -23,8 +24,9 @@ int main(void)
     add_dnodeint_end(&head, 402);
     add_dnodeint_end(&head, 1024);
     print_dlistint(head);
-    node = get_dnodeint_at_index(head, 7);
-    printf("%d\n", node->n);
+    printf("-----------------\n");
+    insert_dnodeint_at_index(&head, 5, 4096);
+    print_dlistint(head);
     free_dlistint(head);
     head = NULL;
     return (EXIT_SUCCESS);
