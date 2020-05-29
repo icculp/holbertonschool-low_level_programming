e91432c06ac913660185f48142151b98633a2e7f
Ian Culp <icculp@gmail.com>
2020-05-29 23:23:38 +0000

    Compiling but not finished

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,58 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-
-shash_table_t *shash_table_create(unsigned long int size);
-int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-char *shash_table_get(const shash_table_t *ht, const char *key);
-void shash_table_print(const shash_table_t *ht);
-void shash_table_print_rev(const shash_table_t *ht);
-void shash_table_delete(shash_table_t *ht);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
-unsigned long int hash_djb2(const unsigned char *str);
-
+#include "hash_tables.h"
 
 /**
- * struct shash_node_s - Node of a sorted hash table
- *
- * @key: The key, string
- * The key is unique in the HashTable
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
- * @sprev: A pointer to the previous element of the sorted linked list
- * @snext: A pointer to the next element of the sorted linked list
- */
-typedef struct shash_node_s
-{
-     char *key;
-     char *value;
-     struct shash_node_s *next;
-     struct shash_node_s *sprev;
-     struct shash_node_s *snext;
-} shash_node_t;
-
-/**
- * struct shash_table_s - Sorted hash table data structure
- *
- * @size: The size of the array
- * @array: An array of size @size
- * Each cell of this array is a pointer to the first node of a linked list,
- * because we want our HashTable to use a Chaining collision handling
- * @shead: A pointer to the first element of the sorted linked list
- * @stail: A pointer to the last element of the sorted linked list
- */
-typedef struct shash_table_s
-{
-     unsigned long int size;
-     shash_node_t **array;
-     shash_node_t *shead;
-     shash_node_t *stail;
-} shash_table_t;
-
-
-
-
+* shash_table_create - Creates a sorted hash table
+* @size: Size of array to make
+* Return: Created hash table, or NULL on failure
+*/
 
 shash_table_t *shash_table_create(unsigned long int size)
 {
@@ -71,46 +23,6 @@ shash_table_t *shash_table_create(unsigned long int size)
 	return (created);
 }
 
-
-/**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
- *
- * Return: hash value
- */
-
-unsigned long int hash_djb2(const unsigned char *str)
-{
-	unsigned long int hash;
-	int c;
-
-	hash = 5381;
-	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c;
-	}
-	return (hash);
-}
-
-
-/**
-* key_index - Returns the index of a given key
-* @key: Key of key/value pair
-* @size: Size of array in hash table
-* Return: Index of key
-*/
-
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
-{
-	unsigned long int i = hash_djb2(key);
-
-	i %= size;
-	return (i);
-}
-
-
-int shash_table_set(shash_table_t *ht, const char *key, const char *value)
-{
 
 /**
 * shash_table_set - Adds an element to a shash table
@@ -163,6 +75,7 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 	return (1);
 }
 
+
 /**
 * shash_table_get - Retrieves the value of a given key
 * @ht: Head of hashtable
@@ -211,18 +124,19 @@ void shash_table_print(const shash_table_t *ht)
 
 	if (ht == NULL)
 		return;
-	print("{");
-	temp = ht->stail;
+	printf("{");
+	temp = ht->shead;
 	while (temp != NULL)
 	{
 		if (count > 0)
 			printf(", ");
 		printf("'%s': '%s'", temp->key, temp->value);
-		temp = temp->sprev;
+		temp = temp->snext;
 		count++;
 	}
 	printf("}\n");
 }
+
 
 /**
 * shash_table_print_rev - Prints a shash table reversed
@@ -236,7 +150,7 @@ void shash_table_print_rev(const shash_table_t *ht)
 
 	if (ht == NULL)
 		return;
-	print("{");
+	printf("{");
 	temp = ht->stail;
 	while (temp != NULL)
 	{
@@ -249,6 +163,7 @@ void shash_table_print_rev(const shash_table_t *ht)
 	printf("}\n");
 }
 
+
 /**
 * hash_table_delete - Deletes a hash table
 * @ht: Head of table
@@ -256,7 +171,7 @@ void shash_table_print_rev(const shash_table_t *ht)
 
 void shash_table_delete(shash_table_t *ht)
 {
-	hash_node_t *a, *t;
+	shash_node_t *a, *t;
 	unsigned long int i;
 
 	if (ht == NULL)
