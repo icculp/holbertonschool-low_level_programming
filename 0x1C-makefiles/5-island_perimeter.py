c079fc2d31548cd8c8491c76d4f766aa69931175
Ian Culp <icculp@gmail.com>
2020-07-06 22:53:44 +0000

    Adding checks for grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,6 +7,8 @@
 def island_perimeter(grid):
     """ returns perimeter of island """
     perimeter = 0
+    if grid is None or len(grid) == 0 or len(grid[0]) == 0:
+        return perimeter
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1 and grid[i + 1][j] == 0:
