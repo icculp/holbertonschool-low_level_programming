12e5ba4f5eb747c4346e79678eadfc89658c8e0f
Ian Culp <icculp@gmail.com>
2020-07-06 23:29:23 +0000

    Edges surround grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,15 +9,25 @@ def island_perimeter(grid):
     perimeter = 0
     if grid is None or len(grid) == 0 or len(grid[0]) == 0:
         return perimeter
-    for i in range(1, len(grid) - 1):
-        for j in range(1, len(grid[i]) - 1):
-            if grid[i][j] == 1 and grid[i + 1][j] == 0:
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if i == 0 and grid[i][j] == 1:
                 perimeter += 1
-            if grid[i][j] == 1 and grid[i - 1][j] == 0:
+            if j == 0 and grid[i][j] == 1:
                 perimeter += 1
-            if grid[i][j] == 1 and grid[i][j + 1] == 0:
+            if j == len(grid[0]) - 1 and grid[i][j] == 1:
                 perimeter += 1
-            if grid[i][j] == 1 and grid[i][j - 1] == 0:
+            if i == len(grid) - 1 and grid[i][j] == 1:
+                perimeter += 1
+
+            if i != len(grid) - 1 and grid[i][j] == 1 and grid[i + 1][j] == 0:
+                perimeter += 1
+            if i != 0 and grid[i][j] == 1 and grid[i - 1][j] == 0:
+                perimeter += 1
+            if j != len(grid[i]) - 1 and grid[i][j] == 1\
+               and grid[i][j + 1] == 0:
+                perimeter += 1
+            if j != 0 and grid[i][j] == 1 and grid[i][j - 1] == 0:
                 perimeter += 1
             if j == 100:
                 break
